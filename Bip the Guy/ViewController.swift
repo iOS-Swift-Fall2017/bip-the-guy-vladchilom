bbee259c6c01a01d985ef92f72451981ceb07487
Vlad Chilom <vchilom6@gmail.com>
2017-09-19 16:33:26 -0400

    Through 4.3

--- Bip the Guy/ViewController.swift
+++ Bip the Guy/ViewController.swift
@@ -7,19 +7,47 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
+  
+  
+  @IBOutlet weak var imageToPunch: UIImageView!
+  var audioPlayer = AVAudioPlayer()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  
+  func animateImage() {
+    let bounds = self.imageToPunch.bounds
+    let shrinkValue: CGFloat = 60
+    self.imageToPunch.bounds = CGRect(x: self.imageToPunch.bounds.origin.x + shrinkValue, y: self.imageToPunch.bounds.origin.y + shrinkValue, width: self.imageToPunch.bounds.size.width - shrinkValue, height: self.imageToPunch.bounds.size.height - shrinkValue)
+    UIView.animate(withDuration: 0.25, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10, options: [], animations: { self.imageToPunch.bounds = bounds }, completion: nil)
   }
-
-
+  
+  func playSound(soundName: String, audioPlayer: inout AVAudioPlayer) {
+    if let sound = NSDataAsset(name: soundName) {
+      do {
+        try audioPlayer = AVAudioPlayer(data: sound.data)
+        audioPlayer.play()
+      } catch {
+        print("ERROR: Data from \(soundName) could not be played")
+      }
+    } else {
+      print("ERROR: Could not load data from file \(soundName)")
+    }
+  }
+  
+  @IBAction func libraryPressed(_ sender: UIButton) {
+  }
+  
+  @IBAction func cameraPressed(_ sender: UIButton) {
+  }
+  
+  @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
+    animateImage()
+    playSound(soundName: "punchSound", audioPlayer: &audioPlayer)
+  }
+  
 }
-
